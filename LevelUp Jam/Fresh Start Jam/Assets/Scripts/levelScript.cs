1a08c9ee54e541ee1ea7e74a6beb135fb0358f5b
Natalie <natalie-mitchell1@hotmail.co.uk>
2021-01-01 23:05:23 +0000

    Player Movement
    Player moves. Onto making good sprites and enemy.
    Need to add player health etc

---
+++ LevelUp Jam/Fresh Start Jam/Assets/Scripts/levelScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This script will handle the progress of the stage through how much the player has restored
+    // Progress bar
+
+    // Timer (for time bonus?)
+
+    // Canvas stuff?
+
+public class levelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LevelUp Jam/Fresh Start Jam/Assets/Scripts/playerMovementScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementScript : MonoBehaviour
+{
+    public float speed, jumpForce; // Movement values
+    private int lookDir; // the direction of the sprite changes based on the button pressed
+    public Rigidbody2D player;
+    bool bFacingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bFacingRight = true;
+        speed = 1;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Check the player movement
+        checkMovement();
+    }
+
+    void checkMovement()
+    {
+        //Left
+        if (Input.GetKey("a"))
+        {
+            //flip if not facing left
+            if (bFacingRight)
+            {
+                transform.Rotate(0, -180, 0, Space.Self);
+                bFacingRight = !bFacingRight;
+            }
+            // move by speed value
+            player.velocity = new Vector2(-speed, player.velocity.y);
+        }
+        //Right
+        if (Input.GetKey("d"))
+        {
+            // flip if not facing right
+            if (!bFacingRight)
+            {
+                transform.Rotate(0, 180, 0, Space.Self);
+                bFacingRight = !bFacingRight;
+            }
+            // move by speed value
+            player.velocity = new Vector2(speed, player.velocity.y);
+        }
+        //Jump
+        if (Input.GetKey("space"))
+        {
+        
+
+        }
+    }
+}
