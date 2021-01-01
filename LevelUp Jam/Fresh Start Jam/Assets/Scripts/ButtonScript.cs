c41ec73e7d44f7cfb5ee4bfc7760cf57695ce858
Natalie <natalie-mitchell1@hotmail.co.uk>
2021-01-01 21:26:43 +0000

    Tried making audio transfer
    Audio slider and mute dont transfer stats over to other stages > need to for settings.
    Created empty sprites for assets in level stage

--- LevelUp Jam/Fresh Start Jam/Assets/Scripts/ButtonScript.cs
+++ LevelUp Jam/Fresh Start Jam/Assets/Scripts/ButtonScript.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
+
+    public Canvas optionsCanvas, creditsCanvas; // Options and credits canvas references.
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set the top canvases to disabled on start to show the main menu:
+        optionsCanvas.enabled = false;
+        creditsCanvas.enabled = false;
+ 
+
     }
 
     // Update is called once per frame
@@ -15,4 +25,15 @@ public class ButtonScript : MonoBehaviour
     {
         
     }
+
+    // Menu UI Events
+
+    // Scene Changing Events
+
+    public void onStartButton() 
+    {
+        SceneManager.LoadScene("Levels");
+    }
 }
+
+
---
+++ LevelUp Jam/Fresh Start Jam/Assets/Scripts/audioScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class audioScript : MonoBehaviour
+{
+    public Slider volumeSlider; // Volume slider element in the settings canvas
+    public AudioSource audioSource; // The audio source to produce music and sound effects within the game.
+    public float fVolume = 1;
+    public bool bMuted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onVolumeSlide()
+    {
+        fVolume = volumeSlider.value;
+        audioSource.volume = fVolume;
+
+    }
+    public void onMuteButtonPressed()
+    {
+        audioSource.mute = !audioSource.mute;
+    }
+
+}
+
+
+
